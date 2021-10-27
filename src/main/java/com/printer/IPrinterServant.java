6030f8dfa1a022a74db24e50e2a35e5b9883d46a
Max <maxtnielsen@gmail.com>
2021-10-27 20:30:05 +0200

    minor change

--- src/main/java/com/printer/IPrinterServant.java
+++ src/main/java/com/printer/IPrinterServant.java
@@ -5,27 +5,27 @@ import java.rmi.RemoteException;
 
 public interface IPrinterServant extends Remote {
 
-    public void print(String filename, String printer, String cookie) throws RemoteException;
+    void print(String filename, String printer, String cookie) throws RemoteException;
 
-    public void queue(String printer, String cookie) throws RemoteException;
+    void queue(String printer, String cookie) throws RemoteException;
 
-    public void topQueue(String printer, int job, String cookie) throws RemoteException, InterruptedException;
+    void topQueue(String printer, int job, String cookie) throws RemoteException, InterruptedException;
 
-    public String start(String password, String username) throws RemoteException;
+    String start(String password, String username) throws RemoteException;
 
-    public void stop(String cookie) throws RemoteException, InterruptedException;
+    void stop(String cookie) throws RemoteException, InterruptedException;
 
-    public void restart(String cookie) throws RemoteException;
+    void restart(String cookie) throws RemoteException;
 
-    public void status(String printer, String cookie) throws RemoteException;
+    void status(String printer, String cookie) throws RemoteException;
 
-    public void readConfig(String parameter, String cookie) throws RemoteException;
+    void readConfig(String parameter, String cookie) throws RemoteException;
 
-    public void setConfig(String parameter, String value, String cookie) throws RemoteException;
+    void setConfig(String parameter, String value, String cookie) throws RemoteException;
 
-    public boolean checkTimeStamp(Cookie c) throws RemoteException;
+    boolean checkTimeStamp(Cookie c) throws RemoteException;
 
-    public boolean authenticateCookie(String cookie) throws RemoteException;
+    boolean authenticateCookie(String cookie) throws RemoteException;
 
-    public void initialisePrinters() throws RemoteException;
+    void initialisePrinters() throws RemoteException;
 }
